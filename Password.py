9a32aac806bb6faa6b6357821c8f2d4727b61a10
Sakhile-Mbane <96161327+Sakhile-Mbane@users.noreply.github.com>
2023-06-28 16:47:52 +0200

    Add files via upload

---
+++ Password.py
@@ -0,0 +1,42 @@
+import random
+import sqlite3
+
+# Connect to the database
+conn = sqlite3.connect('/Users/sakhile/Documents/Projects/Pass/DB/passwords.db')
+c = conn.cursor()
+
+# Create table if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS passwords
+             (platform text, password text)''')
+
+char = 'abcdefghijklmnopqrsABCDEFGHIJKLMNOPQRS!@#$%^&.(),'
+number = int(input('The number of passwords to generate: '))
+length = int(input('Length of password: '))
+
+for i in range(number):
+    platform = input('Enter the platform for the password: ')
+    password = ''
+    for j in range(length):
+        password += random.choice(char)
+    
+    # Insert the password into the database
+    c.execute("INSERT INTO passwords VALUES (?, ?)", (platform, password))
+    conn.commit()
+
+print('Passwords stored successfully.')
+
+# Retrieve and display the most recent password
+c.execute("SELECT * FROM passwords ORDER BY ROWID DESC LIMIT 1")
+result = c.fetchone()
+
+if result:
+    platform, password = result
+    print('Most recently stored password:')
+    print('Platform:', platform)
+    print('Password:', password)
+else:
+    print('No passwords found in the database.')
+
+# Close the database connection
+conn.close()
+
