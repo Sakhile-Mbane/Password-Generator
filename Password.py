e3db9b50dca024eddfafe29552d87cc0d3d56258
Sakhile-Mbane <96161327+Sakhile-Mbane@users.noreply.github.com>
2023-06-29 10:25:41 +0200

    Update Password.py

--- Password.py
+++ Password.py
@@ -7,7 +7,7 @@ c = conn.cursor()
 
 # Create table if it doesn't exist
 c.execute('''CREATE TABLE IF NOT EXISTS passwords
-             (platform text, password text)''')
+             (platform text, username text, password text)''')
 
 char = 'abcdefghijklmnopqrsABCDEFGHIJKLMNOPQRS!@#$%^&.(),'
 number = int(input('The number of passwords to generate: '))
@@ -15,28 +15,29 @@ length = int(input('Length of password: '))
 
 for i in range(number):
     platform = input('Enter the platform for the password: ')
+    username = input('Enter the username for the password: ')
     password = ''
     for j in range(length):
         password += random.choice(char)
-    
+
     # Insert the password into the database
-    c.execute("INSERT INTO passwords VALUES (?, ?)", (platform, password))
+    c.execute("INSERT INTO passwords (platform, username, password) VALUES (?, ?, ?)", (platform, username, password))
     conn.commit()
 
 print('Passwords stored successfully.')
 
-# Retrieve and display the most recent password
+# Retrieve and display the most recently stored password
 c.execute("SELECT * FROM passwords ORDER BY ROWID DESC LIMIT 1")
 result = c.fetchone()
 
 if result:
-    platform, password = result
+    platform, username, password = result
     print('Most recently stored password:')
     print('Platform:', platform)
+    print('Username:', username)
     print('Password:', password)
 else:
     print('No passwords found in the database.')
 
 # Close the database connection
 conn.close()
-
